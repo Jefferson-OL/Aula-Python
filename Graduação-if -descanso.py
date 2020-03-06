fdd23ebc2e79aff509f8352ff6fc6f0f79e9f0d1
Jefferson-OL <61586734+Jefferson-OL@users.noreply.github.com>
2020-03-06 18:45:03 -0300

    Add files via upload

---
+++ Graduação-if -descanso.py
@@ -0,0 +1,19 @@
+graduacao = input("Qual a graduação que você realiza tecnologo ou bacharelado?").upper()
+
+if graduacao!= "BACHARELADO" and graduacao!= "TECNOLOGO":
+    graduacao = input("Diga somente a modalidade (bacharelado ou tecnologo)").upper()
+if graduacao == "BACHARELADO":
+    print("O curso tem de 4 a 6 anos de duração")
+
+else:
+    print("O curso tem de 2 a 3 anos de duração")
+
+
+
+
+ 
+    
+
+
+
+
